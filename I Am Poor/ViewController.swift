bbac9c35383f407d63fa794d58525b7671a0823c
Giovanni Almeida <69661645+GiovanniBot@users.noreply.github.com>
2023-02-03 23:21:07 -0300

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Giovanni Almeida Soares on 03/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
